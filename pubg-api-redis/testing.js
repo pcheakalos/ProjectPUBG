a86fd9a2e9c3bd914905a008ef2abc3d6fbe3fff
HackedK <kunkim0619@gmail.com>
2017-11-19 15:23:39 -0500

    pubg.js to retrieve data

--- pubg-api-redis/testing.js
+++ pubg-api-redis/testing.js
@@ -1,6 +1,6 @@
 const {PubgAPI} = require('./');
 
-const api = new PubgAPI({apikey: process.env.PUBG_APIKEY});
+const api = new PubgAPI('a7d9348b-e8bb-47b0-b9d4-cb1428b8bd01');
 
 api.getProfileByNickname('fak3zito')
   .then((profile) => {
---
+++ pubg.js
@@ -0,0 +1,22 @@
+const {PubgAPI, PubgAPIErrors, REGION, SEASON, MATCH} = require('pubg-api-redis');
+
+// If no Redis configuration it wont be cached
+const api = new PubgAPI({
+  apikey: 'a7d9348b-e8bb-47b0-b9d4-cb1428b8bd01',
+  redisConfig: {
+    host: '127.0.0.1',
+    port: 6379,
+    expiration: 300, // Optional - defaults to 300.
+  },
+});
+
+api.getProfileByNickname('SoAh_117')
+  .then((profile) => {
+    const data = profile.content;
+    const stats = profile.getStats({
+      region: REGION.ALL, // defaults to profile.content.selectedRegion
+      season: SEASON.EA2017pre3, // defaults to profile.content.defaultSeason
+      match: MATCH.SOLO // defaults to SOLO
+    });
+    console.log(stats);
+  });
