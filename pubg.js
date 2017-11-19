00edba3872035bcd533281d5cd516d8dd1456821
HackedK <kunkim0619@gmail.com>
2017-11-19 15:24:39 -0500

    clearing my APIKEY

--- pubg.js
+++ pubg.js
@@ -2,7 +2,7 @@ const {PubgAPI, PubgAPIErrors, REGION, SEASON, MATCH} = require('pubg-api-redis'
 
 // If no Redis configuration it wont be cached
 const api = new PubgAPI({
-  apikey: 'a7d9348b-e8bb-47b0-b9d4-cb1428b8bd01',
+  apikey: 'XXXXXXX',
   redisConfig: {
     host: '127.0.0.1',
     port: 6379,
