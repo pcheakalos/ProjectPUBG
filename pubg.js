d573d1817f10ec90c7bf75573395e61ab6205105
HackedK <kunkim0619@gmail.com>
2017-11-20 19:58:02 -0500

    create DB for the pubg project

--- pubg.js
+++ pubg.js
@@ -1,8 +1,17 @@
 const {PubgAPI, PubgAPIErrors, REGION, SEASON, MATCH} = require('pubg-api-redis');
 
+//var jsonfile = require('jsonfile');
+//var file = '/Users/kunkim/git/ProjectPUBG';
+//var obj = {name: output};
+//
+//jsonfile.writeFileSync(file,obj);
+
+const fs = require('fs');
+
 // If no Redis configuration it wont be cached
 const api = new PubgAPI({
-  apikey: 'XXXXXXX',
+  apikey: 'a7d9348b-e8bb-47b0-b9d4-cb1428b8bd01',
+    //  my api key: a7d9348b-e8bb-47b0-b9d4-cb1428b8bd01
   redisConfig: {
     host: '127.0.0.1',
     port: 6379,
@@ -18,5 +27,11 @@ api.getProfileByNickname('SoAh_117')
       season: SEASON.EA2017pre3, // defaults to profile.content.defaultSeason
       match: MATCH.SOLO // defaults to SOLO
     });
+    if(data != null){
+        var rawData = JSON.stringify(stats, null, 2);
+        fs.writeFile('output.json', rawData);
+    }else{
+        console.log("ID does not exist");
+    }
     console.log(stats);
   });
