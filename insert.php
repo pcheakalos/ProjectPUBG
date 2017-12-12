3e04633a20966f241bfe08321ec5e2eeb9701987
Colin Sullivan <colinsullivan@tests-MacBook-Pro-5.local>
2017-12-12 12:50:49 -0500

    Reset column names

--- insert.php
+++ insert.php
@@ -60,7 +60,7 @@ function query_to_db($conn, $sql){
     }
 }
 
-$sql = "INSERT INTO mydb (player_name, region, season, matches, last_update, rate, rounds, wins, top10, kills, suicides, teamkills, headshots, roadkills, assists)
+$sql = "INSERT INTO mydb (name, region, season, match, update, rate, rounds, wins, top10, kills, suicides, teamkills, headshots, roadkills, assists)
         VALUES ($name, $region, $season, $match, $update, $rate, $rounds, $wins, $top10, $kills, $suicides, $teamkills, $headshots, $roadkills, $assists)";
 
 query_to_db($conn, $sql);
